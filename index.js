accd8bd708e63a5c33db74c90a3bab2bba63a001
Avijit Karmaker <ajkacca@gmail.com>
2021-07-29 01:32:02 +0300

    add items page

--- index.js
+++ index.js
@@ -3,9 +3,21 @@ const url= require("url");
 const fs= require("fs");
 
 const homepage= fs.readFileSync(`${__dirname}/templates/Home.html`);
-const aboutpage= fs.readFileSync(`${__dirname}/templates/About.html`)
+const aboutpage= fs.readFileSync(`${__dirname}/templates/About.html`);
+const itemspage= fs.readFileSync(`${__dirname}/templates/items.html`);
+const templatecard= fs.readFileSync(`${__dirname}/templates/card.html`);
+const individualItem= fs.readFileSync(`${__dirname}/templates/item.html`);
 const itemdata= fs.readFileSync("./items.json", "utf-8");
 const itemObj= JSON.parse(itemdata);
+
+
+const replaceTemplate=(temp,el)=>{
+  let output =temp.toString().replace(/{%name%}/g, el.name);
+  output = output.toString().replace(/{%image%}/g, el.image);
+  output = output.toString().replace(/{%description%}/g, el.description);
+  return output;
+}
+
 
 
 const server= http.createServer((req,res)=>{
@@ -27,9 +39,13 @@ const server= http.createServer((req,res)=>{
 
             case "/items":           
             res.writeHead(200, {
-                "Content-type":"application/json"
+                "Content-type":"text/html"
             })
-            res.end(itemdata)
+            const cardItem= itemObj.map((el)=> replaceTemplate(templatecard,el)).join("");
+            
+            let output= itemspage.toString().replace(/{%card%}/g,cardItem);
+
+            res.end(output);
     
         default:
             res.writeHead(200, {
