d8af1f4e7a406e1b289c595c57d1a243b03f1cb7
Avijit Karmaker <ajkacca@gmail.com>
2021-07-29 13:31:54 +0300

    add individual route

--- index.js
+++ index.js
@@ -7,53 +7,73 @@ const aboutpage= fs.readFileSync(`${__dirname}/templates/About.html`);
 const itemspage= fs.readFileSync(`${__dirname}/templates/items.html`);
 const templatecard= fs.readFileSync(`${__dirname}/templates/card.html`);
 const individualItem= fs.readFileSync(`${__dirname}/templates/item.html`);
+const notfound= fs.readFileSync(`${__dirname}/templates/Notfound.html`);
 const itemdata= fs.readFileSync("./items.json", "utf-8");
 const itemObj= JSON.parse(itemdata);
+
 
 
 const replaceTemplate=(temp,el)=>{
   let output =temp.toString().replace(/{%name%}/g, el.name);
   output = output.toString().replace(/{%image%}/g, el.image);
   output = output.toString().replace(/{%description%}/g, el.description);
+  output= output.toString().replace(/{%id%}/g, el.id)
   return output;
 }
 
 
+const replaceindividual=(template,element)=>{
+
+     let output= template.toString().replace(/{%name%}/g, element.name);
+     output = output.toString().replace(/{%image%}/g, element.image);
+    output = output.toString().replace(/{%description%}/g, element.description);
+    output = output.toString().replace(/{%price%}/g, element.price);
+    output = output.toString().replace(/{%location%}/g, element.Location);
+    output = output.toString().replace(/{%materials%}/g, element.materials);
+    return output;
+
+}
+
 
 const server= http.createServer((req,res)=>{
 
-    const pathname= req.url;
+    const  {query:{id}, pathname} = url.parse(req.url, true);
+    console.log(pathname);
 
-    switch (pathname) {
-        case "/home":           
-            res.writeHead(200, {
-                "Content-type":"text/html"
-            })
-            res.end(homepage)
+    if(pathname==="/" || pathname==="/home"){
+        res.writeHead(200, {
+            "Content-type":"text/html"
+        })
+        res.end(homepage)
+    } else if ( pathname ==="/about") {
+        res.writeHead(200, {
+            "Content-type":"text/html"
+        })
+        res.end(aboutpage)
+    } else if (pathname==="/items"){
+        res.writeHead(200, {
+            "Content-type":"text/html"
+        })
+        const cardItem= itemObj.map((el)=> replaceTemplate(templatecard,el)).join("");
+        
+        let output= itemspage.toString().replace(/{%card%}/g,cardItem);
 
-            case "/about":           
-            res.writeHead(200, {
-                "Content-type":"text/html"
-            })
-            res.end(aboutpage)
+        res.end(output);
+    } else if (pathname ==="/item"){
+        res.writeHead(200,{
+            "Content-type": "text/html"
+        })
+        const singleItem= itemObj[id];
+        let singleoutput= replaceindividual(individualItem,singleItem);
+        res.end(singleoutput);
+    } else {
+        res.writeHead(404,{
+            "Content-type": "text/html"
+        })
+        res.end(notfound);
 
-            case "/items":           
-            res.writeHead(200, {
-                "Content-type":"text/html"
-            })
-            const cardItem= itemObj.map((el)=> replaceTemplate(templatecard,el)).join("");
-            
-            let output= itemspage.toString().replace(/{%card%}/g,cardItem);
-
-            res.end(output);
+    }
     
-        default:
-            res.writeHead(200, {
-                "Content-type":"text/html"
-            })
-            res.end(homepage)
-    }    
-
 })
 
 
